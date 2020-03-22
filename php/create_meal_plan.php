375ce0d654b27b534d87b99785395e7c8d83f697
Kosta Jovanovic <kosta.jovanovic86@gmail.com>
2020-03-22 01:17:30 +0100

    napravljeno da u create_create_meal_plan popunjava sedmodnevni_plan ali bez uticaja kalorija - sledece napraviti da prilagodjava dnevnim potrebama za unos kalorija

--- php/create_meal_plan.php
+++ php/create_meal_plan.php
@@ -6,28 +6,59 @@ $data = json_decode($json_data);
 $obroci = $data->obroci;
 $sedmodnevni_plan = $data->sedmodnevniPlanTemplate;
 
-$iskljuceneNamirnice = 'mast';
+$iskljucene_namirnice = 'mast';
 
 
-var_dump($$sedmodnevni_plan->dan->{'1'});
 
 
 
 function meal_plan($kalorije, $obroci, $sedmodnevni_plan, $iskljucene_namirnice) {
     $dorucak_array = $obroci->dorucak;
+    shuffle($dorucak_array);
+
     $rucak_array = $obroci->rucak;
+    shuffle($rucak_array);
+
     $vecera_array = $obroci->vecera;
+    shuffle($vecera_array);
 
-    foreach ($dorucak as $key => $value) {
+    $dan_iterator = 1;
 
-		//ako bilo koja namirnica iz $iskljuceneNamirnice(array) nije u sastojcima dorucka $obrociJSON->dorucak(array)->[0]->sastojci
-		if (!in_array($iskljuceneNamirnice, $dorucak_array[$key]->sastojci) {
-			$sedmodnevni_plan->dan = (object) ['dorucak' => $value]
+    // popunjavanje dorucka za svaki dan sedmodnevnog plana
+    foreach ($dorucak_array as $key => $value) {
+
+		//ako bilo koja namirnica iz $iskljuceneNamirnice(array) nije u sastojcima obroka koji je trenutno u iteraciji ubacuje taj obrok u plan
+		if (!in_array($iskljucene_namirnice, array($dorucak_array[$key]->sastojci)) && $dan_iterator <= 7) {
+            $sedmodnevni_plan->dan->{$dan_iterator}->dorucak = $value;
+            $dan_iterator++;
 		}
-
     }
 
-    
+    //resetovanje iteratora
+    $dan_iterator = 1;
+
+    // popunjavanje rucka za svaki dan sedmodnevnog plana
+    foreach ($rucak_array as $key => $value) {
+
+        //ako bilo koja namirnica iz $iskljuceneNamirnice(array) nije u sastojcima obroka koji je trenutno u iteraciji ubacuje taj obrok u plan
+        if (!in_array($iskljucene_namirnice, array($rucak_array[$key]->sastojci)) && $dan_iterator <= 7) {
+            $sedmodnevni_plan->dan->{$dan_iterator}->rucak = $value;
+            $dan_iterator++;
+        }
+    }
+
+    //resetovanje iteratora
+    $dan_iterator = 1;
+
+    // popunjavanje rucka za svaki dan sedmodnevnog plana
+    foreach ($vecera_array as $key => $value) {
+
+        //ako bilo koja namirnica iz $iskljuceneNamirnice(array) nije u sastojcima obroka koji je trenutno u iteraciji ubacuje taj obrok u plan
+        if (!in_array($iskljucene_namirnice, array($vecera_array[$key]->sastojci)) && $dan_iterator <= 7) {
+            $sedmodnevni_plan->dan->{$dan_iterator}->vecera = $value;
+            $dan_iterator++;
+        }
+    }
 
 
     var_dump($sedmodnevni_plan);
