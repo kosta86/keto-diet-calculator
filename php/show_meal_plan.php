92014c1c261b2c18edda118d50c7f99384844a02
kosta86 <kosta.jovanovic86@gmail.com>
2020-03-27 18:09:56 +0100

    show meal plan u toku

--- php/show_meal_plan.php
+++ php/show_meal_plan.php
@@ -1,20 +1,30 @@
 <?php
 include_once('database_connection.php');
 
-$query_array = parse_str($_SERVER['QUERY_STRING'], $vars);
-$user_id = filter_var($vars['id'], FILTER_VALIDATE_INT);
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $query_array = parse_str($_SERVER['QUERY_STRING'], $vars);
+    $user_id = filter_var($vars['id'], FILTER_VALIDATE_INT);
 
-if ($user_id) {
+    /* if ($user_id) { */
 
-  var_dump($user_id);
+    var_dump($user_id);
 
-  $query = "SELECT dan FROM korisnici WHERE id = ':user_id' ";
+    $query = "SELECT * FROM korisnici WHERE id=:id ";
 
-  $statement = $connect->prepare($query);
-  $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement = $connect->prepare($query);
+    $statement->bindParam(':id', $user_id, PDO::PARAM_INT);
 
-  $statement->execute();
-  
+    $statement->execute();
+
+    $found_rows = $statement->fetch();
+
+    $dan = $found_rows[4];
+    
+    $query = "UPDATE korisnici SET dan=concat(dan,dan-(dan+1)) WHERE id=$user_id";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+
+    var_dump($found_rows[4]);
 } else {
-  echo 'noquery string';
+    echo 'noquery string';
 }
